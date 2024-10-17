78c83d6aa0a46f4c3c9543e59fed801326d7369b
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2024-10-17 18:23:31 -0300

    revisao

---
+++ aulas/aula01.asm
@@ -0,0 +1,9 @@
+#operações basicas em assembly
+
+#contém 32 registradores ($) de 32 bits, alguns deles possuem uso predeterminado, exemplo: $0 == 0, $31 usado para subrotina (modularização do código) retorno de endereço.
+
+# registradores de uso livre: $8 a $15 e $24 $25 - variaveis temporárias (disponíveis ao usuário), $16 a $23 - variaveis salvas (a subrotina não altera o seu valor)
+# registrador HI e LO - ex: recebem o resultado de uma multiplicação de dois numeros de 32 bits
+
+# operaações aritmeticas
+
