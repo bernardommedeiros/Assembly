9bc02d0e4c693c2c01cb2cae150206c8ba12bce3
bernardommedeiros <161090624+bernardommedeiros@users.noreply.github.com>
2024-10-28 16:00:18 -0300

    revisao

--- aulas/aula01.asm
+++ aulas/aula01.asm
@@ -7,3 +7,31 @@
 
 # operaações aritmeticas
 
+    # soma de dois numeros == add $destino ex:$10, $fonte1 ex:$8, $fonte2 ex:$9
+    # addi para atribuição de constantes
+
+    addi $8, $0, 5  # carregar valor 5 somado ao registrador 0 na variavel $8
+    addi $8, $0, 4  # carregar valor 5 somado ao registrador 0 na variavel $9
+    add $10, $8, $9  # somar os valores na variaveis $8 e $9 e armazenar o resultado na variavel $10
+
+    # subtracao de dois numeros == sub $destino ex:$10, $fonte1 ex:$8, $fonte2 ex:$9
+    subi $8, $0, 5  # carregar valor 5 subtraido do registrador 0 na variavel $8
+    subi $8, $0, 4  # carregar valor 4 subtraido do registrador 0 na variavel $9
+    sub $10, $8, $9  # subtrair os valores na variaveis $8 e $9 e armazenar o resultado na variavel $10
+
+    # multiplicacao de dois numeros == mul $destino ex:$10, $fonte1 ex:$8, $fonte2 ex:$9
+    mul $10, $8, $9  # multiplicar os valores na variaveis $8 e $9 e armazenar o resultado na variavel $10
+    # divisao de dois numeros == div $destino ex:$10, $fonte1 ex:$8, $fonte2 ex:$9
+    
+    div $10, $8, $9  # dividir os valores na variaveis $8 e $9 e armazenar o resultado na variavel $10
+
+
+# operacoes de deslocamento
+
+    # shift left << - desloca o conteudo do registrador para a esquerda pelo numero de bits especificado
+    # shift right >> - desloca o conteudo do registrador para a direita pelo numero de bits especificado
+    # sll - shift left logical - desloca o conteudo do registrador para a esquerda pelo numero de bits especificado sem preenchimento de zeros à direita
+    # srl - shift right logical - desloca o conteudo do registrador para a direita pelo numero de bits especificado sem preenchimento de zeros à esquerda
+    # sra - shift right arithmetic - desloca o conteudo do registrador para a direita pelo numero de bits especificado com preenchimento de zeros à esquerda
+
+
