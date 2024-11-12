b6a2570e8101a3146202b05eb36e9d8f6d4e2652
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2024-11-12 13:34:55 -0300

    Update revisao_loop.asm

--- aulas/revisao_loop.asm
+++ aulas/revisao_loop.asm
@@ -1,1 +1,15 @@
+EXEMPLO
 
+#inicialiação
+addi $9, $0, 4 #condição de parada
+add $8, $0, $0 #primeira instrução do laço
+
+#teste
+beq $8, $9, sai
+
+
+
+sai:
+
+#incremento
+#adiciona 1 até o $9 chegar a 4
